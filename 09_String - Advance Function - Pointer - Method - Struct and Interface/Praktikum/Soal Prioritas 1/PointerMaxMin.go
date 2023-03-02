fdeb610b6a930fa9268b461ba15c2eeff966d6c3
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-02 14:36:31 +0700

    Update Praktikum

--- 09_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/Soal Prioritas 1/PointerMaxMin.go
+++ 09_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/Soal Prioritas 1/PointerMaxMin.go
@@ -19,19 +19,13 @@ func PointerMaxMin(n ...*int) (max, min int) {
 func main() {
 	var a, b, c, d, e, f int = 1, 2, 3, 9, 8, 7
 	var max, min int
-	// fmt.Scan(&a)
-	// fmt.Scan(&b)
-	// fmt.Scan(&c)
-	// fmt.Scan(&d)
-	// fmt.Scan(&e)
-	// fmt.Scan(&f)
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+	fmt.Scan(&e)
+	fmt.Scan(&f)
 	min, max = PointerMaxMin(&a, &b, &c, &d, &e, &f)
-	fmt.Printf("%d\n", a)
-	fmt.Printf("%d\n", b)
-	fmt.Printf("%d\n", c)
-	fmt.Printf("%d\n", d)
-	fmt.Printf("%d\n", e)
-	fmt.Printf("%d\n", f)
 	fmt.Println("Nilai terendah :", min)
 	fmt.Println("Nilai tertinggi :", max)
 }
