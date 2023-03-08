34896d939088e231fe0ca43d078c6dbd6470a56e
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-08 20:40:01 +0700

    Update Praktikum

--- 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/FiboDeret.go
+++ 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/FiboDeret.go
@@ -2,40 +2,45 @@ package main
 
 import "fmt"
 
-func fibo(n int) int {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 1
-	} else {
-		return fibo(n-1) + fibo(n-2)
+func Fibo(n int, list map[int]int) int {
+	if n < 2 {
+		return n
 	}
+	if val, ok := list[n]; ok {
+		return val
+	}
+
+	list[n] = Fibo(n-1, list) + Fibo(n-2, list)
+	return list[n]
 }
 
-func sumFibo(n int) int {
+func sumFibTopDown(n int, list map[int]int) int {
 	var result int
 	for i := 0; i <= n; i++ {
 		if i == 8 {
 			continue
 		}
-		result += fibo(i)
+		result += Fibo(i, list)
 	}
 	return result
 }
 
 func main() {
-	fmt.Println(fibo(0))
-	fmt.Println(fibo(1))
-	fmt.Println(fibo(2))
-	fmt.Println(fibo(3))
-	fmt.Println(fibo(4))
-	fmt.Println(fibo(5))
-	fmt.Println(fibo(6))
-	fmt.Println(fibo(7))
-	fmt.Println(fibo(9))
-	fmt.Println(fibo(10))
+	fmt.Print("fibTopDown: \n")
+	fmt.Println(Fibo(0, map[int]int{}))
+	fmt.Println(Fibo(1, map[int]int{}))
+	fmt.Println(Fibo(2, map[int]int{}))
+	fmt.Println(Fibo(3, map[int]int{}))
+	fmt.Println(Fibo(4, map[int]int{}))
+	fmt.Println(Fibo(5, map[int]int{}))
+	fmt.Println(Fibo(6, map[int]int{}))
+	fmt.Println(Fibo(7, map[int]int{}))
+	fmt.Println(Fibo(9, map[int]int{}))
+	fmt.Println(Fibo(10, map[int]int{}))
+
 	fmt.Println()
 
-	fmt.Print("Sum of Fibo: \n")
-	fmt.Println(sumFibo(10))
+	fmt.Print("sumFibTopDown: \n")
+	fmt.Println(sumFibTopDown(10, map[int]int{}))
+
 }
