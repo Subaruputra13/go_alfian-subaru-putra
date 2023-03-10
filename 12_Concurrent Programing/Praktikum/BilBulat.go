b58b7c60af09304e69bbebc2d00517fbac925d46
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-10 23:40:29 +0700

    Update Praktikum

---
+++ 12_Concurrent Programing/Praktikum/BilBulat.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var start time.Time
+
+func init() {
+	start = time.Now()
+}
+
+func getBilBulat(x int) {
+	for i := 1; i <= x; i++ {
+		if x%i == 0 {
+			fmt.Printf("%d at time %v\n", i, time.Since(start))
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("main execution started at time", time.Since(start))
+	fmt.Println()
+
+	go getBilBulat(14)
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("\nmain execution ended at time", time.Since(start))
+}
