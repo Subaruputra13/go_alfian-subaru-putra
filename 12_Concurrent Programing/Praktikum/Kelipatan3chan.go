dd70aeacb67b49c75bdca8a5cd19127c87581fec
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-10 23:50:38 +0700

    Update Praktikum

---
+++ 12_Concurrent Programing/Praktikum/Kelipatan3chan.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func kelipatan(kel chan int) {
+	ang := <-kel
+
+	for i := 1; i <= ang; i++ {
+		if i%3 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Start Main")
+	c := make(chan int)
+
+	go kelipatan(c)
+	c <- 20
+
+	fmt.Println("End Main")
+}
