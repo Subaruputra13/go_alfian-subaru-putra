5ac2cd6a7819430bdaaa2effb317a661f55fa8a7
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-10 23:57:52 +0700

    Update Praktikum

--- 12_Concurrent Programing/Praktikum/BilBulat.go
+++ 12_Concurrent Programing/Praktikum/BilBulat.go
@@ -13,7 +13,7 @@ func init() {
 
 func getBilBulat(x int) {
 	for i := 1; i < 50; i++ {
-		if i%7 == 0 {
+		if i%x == 0 {
 			fmt.Printf("%d at time %v\n", i, time.Since(start))
 			time.Sleep(10 * time.Millisecond)
 		}
@@ -24,8 +24,8 @@ func main() {
 	fmt.Println("main execution started at time", time.Since(start))
 	fmt.Println()
 
-	go getBilBulat(14)
+	go getBilBulat(7)
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(100 * time.Millisecond)
 	fmt.Println("\nmain execution ended at time", time.Since(start))
 }
