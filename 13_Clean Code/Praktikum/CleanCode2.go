6c69380f6791b72c73a05171afb93e3ede7a25de
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-12 13:41:29 +0700

    Update Praktikum

--- 13_Clean Code/Praktikum/CleanCode2.go
+++ 13_Clean Code/Praktikum/CleanCode2.go
@@ -1,14 +1,17 @@
 package main
 
+// penggunaan nama struct harus diawali huruf besar dan isinya harus menggunakan PascalCase
 type Kendaraan struct {
 	TotalRoda      int
 	KecepatanMobil int
 }
 
+// penggunaan nama struct harus diawali huruf besar
 type Mobil struct {
 	Kendaraan
 }
 
+// membuat variable harus jelas dan mudah dipahami, penamaan funsi harus menggunakan CamelCase
 func (mobil *Mobil) tambahKecepatan(kecepatanbaru int) {
 	mobil.KecepatanMobil = mobil.KecepatanMobil + kecepatanbaru
 }
@@ -18,6 +21,7 @@ func (mobil *Mobil) berjalan() {
 }
 
 func main() {
+	// Penamaan Variable harus jelas dan mudah dipahami
 	MobilCepat := Mobil{Kendaraan{4, 60}}
 	MobilCepat.berjalan()
 	MobilCepat.berjalan()
