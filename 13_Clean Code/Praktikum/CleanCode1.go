d51c697d50f50de0f49cb6c29145f91f15dd8ab2
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-03-27 16:39:52 +0700

    Update Praktikum

--- 13_Clean Code/Praktikum/CleanCode1.go
+++ 13_Clean Code/Praktikum/CleanCode1.go
@@ -2,25 +2,25 @@ package main
 
 //Penamaan struct harus diawali dengan huruf kapital
 type User struct {
-	id       int
-	username int
-	password int
+	Id       int
+	Username int
+	Password int
 }
 
 //Penamaan struct harus diawali dengan huruf kapital dan harus menggunakan PascalCase dan penamaan variable jangan terlalu singkat
 type UserService struct {
-	tipe []User
+	Tipe []User
 }
 
 //penamaan function harus menggunakan camelCase
 func (user UserService) getAllUsers() []User {
-	return user.tipe
+	return user.Tipe
 }
 
 //penamaan function harus menggunakan camelCase
 func (user UserService) getUserById(id int) User {
-	for _, value := range user.tipe {
-		if id == value.id {
+	for _, value := range user.Tipe {
+		if id == value.Id {
 			return value
 		}
 	}
